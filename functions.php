ec6ae94392feffcd87c39051846fa75253b33024
Ted Waller <ted@tbwiii.com>
2013-09-10 20:50:42 -0400

    registered menus

--- functions.php
+++ functions.php
@@ -2,12 +2,20 @@
 
 init();
 
-function register_styles () {
+function prime1_register_styles () {
 	wp_enqueue_style( "main-styles", get_template_directory_uri() . '/css/mq.css', 'false', 1 );
 }
 
-function init () {
-	add_action('init', 'register_styles');
+function prime1_register_menus () {
+	register_nav_menus( array(
+		'main_menu' => 'Main navigation',
+		'footer_menu' => 'Footer Navigation'
+	) );
+}
+
+function prime1_init () {
+	add_action('init', 'prime1_register_styles');
+	add_action('init', 'prime1_register_menus');
 }
 
 
